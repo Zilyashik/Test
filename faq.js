f6ac1e30c0a4d86c320df2768a70ce7a8ab6951e
Zilyashik <gzilyam17@mail.ru>
2022-04-03 17:05:43 +0300

    FAQ is ready

--- faq.js
+++ faq.js
@@ -1,6 +1,19 @@
 'use strict'
+
 $(document).ready(function() {
-    $('block__title').click(function (event) {
+    $('.block__title').click(function (event) {
+        if($('.block').hasClass('one')){
+            $('.block__title').not($(this)).removeClass('active');
+            $('.block__item').not($(this)).removeClass('active');
+            $('.block__text').not($(this).next()).slideUp(300);
+        }
         $(this).toggleClass('active').next().slideToggle(300);
+
+    });
+});
+
+$(document).ready(function() {
+    $('.block__item').click(function (event) {
+        $(this).toggleClass('active');
     });
 });
