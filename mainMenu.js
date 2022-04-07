3298ec988dc4ffcaa5a08e14175280fc7b0c05eb
Zilyashik <gzilyam17@mail.ru>
2022-04-07 21:43:33 +0300

    everything is ready

--- mainMenu.js
+++ mainMenu.js
@@ -32,3 +32,53 @@ if (isMobile.any()) {
     document.body.classList.add('_pc');
 }
 
+let data_num;
+
+function showNavMenu(event) {
+    if (data_num !== undefined) {
+        closeNavMenu();
+    }
+    let num = getDataNum(event);
+    console.log(typeof num, typeof data_num)
+
+    if (num !== data_num) {
+        data_num = num;
+
+        show(['subMenu_' + num, 'black', 'fade', 'menu_link_' + num], true);
+        return;
+    }
+    data_num = undefined;
+}
+
+function closeNavMenu() {
+    show(['subMenu_' + data_num, 'black', 'fade', 'menu_link_' + data_num], false);
+}
+
+function show(idArray, show) {
+    idArray.map(function (value) {
+        let elem = document.getElementById(value);
+        if (show) {
+            elem.classList.add('move');
+        } else {
+            elem.classList.remove('move');
+        }
+    });
+}
+
+function getDataNum(event) {
+    return event.target.attributes['data-num'].value;
+}
+
+$(document).ready(function () {
+    $('.icon').click(function (event) {
+        $('.icon').toggleClass('active');
+        $('.fade').toggleClass('active');
+        $('.menu__body').toggleClass('active');
+    });
+
+    $('.exit').click(function (event) {
+        $('.icon').removeClass('active');
+        $('.fade').removeClass('active');
+        $('.menu__body').removeClass('active');
+    });
+});
